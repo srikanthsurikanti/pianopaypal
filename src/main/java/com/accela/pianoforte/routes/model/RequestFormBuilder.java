8706a8767127266e0d22282ce662171bf58ce46d
royphillips <royphillips@ebay.com>
2020-04-06 11:10:03 +0100

    Simplified form builder, cleaned-up tests

--- src/main/java/com/accela/pianoforte/routes/model/RequestFormBuilder.java
+++ src/main/java/com/accela/pianoforte/routes/model/RequestFormBuilder.java
@@ -34,26 +34,19 @@ public class RequestFormBuilder {
     }
 
     public Map<String,String> build(final Request request) {
-        return formData(
-                request.getFirstName(), request.getLastName(),
-                UTCTicks.getUtcTime(timestamper.get()).toString(),
-                request.getTransactionId(),
-                request.getAmount().toString());
-    }
-
-    private Map<String, String> formData(final String firstName, final String lastName,
-                                         final String utcTime, final String txOrderNb, final String amount) {
+        final String utcTime = UTCTicks.getUtcTime(timestamper.get()).toString();
         final String pgTsHash = calculateHash(
-                apiLoginId, transactionType, amount, utcTime, txOrderNb, apiVersion);
+                apiLoginId, transactionType, request.getAmount().toString(), utcTime,
+                request.getTransactionId(), apiVersion);
         return ImmutableMap.<String, String>builder()
-                .put("pg_billto_postal_name_first", firstName)
-                .put("pg_billto_postal_name_last", lastName)
-                .put("pg_api_login_id", apiLoginId)
+                .put("pg_billto_postal_name_first", request.getFirstName())
+                .put("pg_billto_postal_name_last", request.getLastName())
+                .put("pg_api_login_id",apiLoginId)
                 .put("pg_transaction_type", transactionType)
                 .put("pg_version_number", apiVersion)
-                .put("pg_total_amount", amount)
+                .put("pg_total_amount", request.getAmount().toString())
                 .put("pg_utc_time", utcTime)
-                .put("pg_transaction_order_number", txOrderNb)
+                .put("pg_transaction_order_number", request.getTransactionId())
                 .put("pg_ts_hash", pgTsHash)
                 .put("pg_return_url", returnUrl)
                 .put("pg_continue_url", continueUrl)
--- src/test/java/PaymentLiveTest.java
+++ src/test/java/PaymentLiveTest.java
@@ -1,23 +1,15 @@
 import com.accela.pianoforte.routes.ApiRoute;
 import com.accela.pianoforte.routes.PaymentRoute;
-import com.fasterxml.jackson.databind.JsonNode;
 import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.test.junit5.CamelTestSupport;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpRequest;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.sql.Timestamp;
 
 public class PaymentLiveTest extends CamelTestSupport {
+    private static final String endpoint = "http://localhost:9090/pianoforte/api/payment/checkout";
     private static final Timestamp timestamp = new Timestamp(System.currentTimeMillis());
     private static final String txOrderNb = Long.toString(timestamp.getTime());
     private static final String jsonRequest =
@@ -28,8 +20,8 @@ public class PaymentLiveTest extends CamelTestSupport {
     @Test
     @DisplayName("API call succeeds")
     public void testPaymentRequestProviderNotConfigured() {
-        final String result = template.requestBodyAndHeader("http://localhost:9090/pianoforte/api/payment/checkout", jsonRequest,
-                Exchange.CONTENT_TYPE, "application/json", String.class);
+        final String result = template.requestBodyAndHeader(
+                endpoint, jsonRequest, Exchange.CONTENT_TYPE, "application/json", String.class);
         System.out.println("result="+result);
     }
 
